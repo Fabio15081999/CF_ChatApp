7ef86628520163f2b8194093a7ef7436eb6dd9da
Fabio <pdphong17@gmail.com>
2021-08-05 16:51:03 +0700

    update login with facebook

--- app/src/main/java/com/example/cf_chatapp/Login.java
+++ app/src/main/java/com/example/cf_chatapp/Login.java
@@ -17,13 +17,25 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import com.facebook.FacebookSdk;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -32,20 +44,25 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 public class Login extends AppCompatActivity {
-    private  Toolbar toolbar;
+    private Toolbar toolbar;
     EditText edtEmailLogin, edtPasswordLogin;
     Button btnLogin;
     private FirebaseAuth mAuth;
     DatabaseReference reference;
     FirebaseUser firebaseUser;
     TextView tvResetPass;
+    private DatabaseReference mReference;
     private ProgressBar progressBar;
+    CallbackManager callbackManager;
+//    String email, firstname;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        callbackManager = CallbackManager.Factory.create();
+
         toolbar = (Toolbar) findViewById(R.id.toolbarLogin);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
@@ -68,8 +85,8 @@ public class Login extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
 
-        edtEmailLogin = (EditText) findViewById(R.id.edtUsernameMain) ;
-        edtPasswordLogin =(EditText) findViewById(R.id.edtPassWord);
+        edtEmailLogin = (EditText) findViewById(R.id.edtUsernameMain);
+        edtPasswordLogin = (EditText) findViewById(R.id.edtPassWord);
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,11 +94,11 @@ public class Login extends AppCompatActivity {
                 String email = edtEmailLogin.getText().toString().trim();
                 String pass = edtPasswordLogin.getText().toString().trim();
 
-                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pass) ){
+                if (TextUtils.isEmpty(email) || TextUtils.isEmpty(pass)) {
                     Toast.makeText(Login.this, "khong duoc de trong cac muc ", Toast.LENGTH_SHORT).show();
-                }else if (pass.length() <6){
+                } else if (pass.length() < 6) {
                     Toast.makeText(Login.this, "pass phai lon hon 6 ky tu!", Toast.LENGTH_SHORT).show();
-                }else {
+                } else {
                     login(email, pass);
 
                 }
@@ -89,8 +106,81 @@ public class Login extends AppCompatActivity {
         });
         progressBar = findViewById(R.id.progress_bar_login);
 
+        LoginButton loginButton = findViewById(R.id.login_facebook_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+            }
+        });
+
 
     }
+
+    private void handleFacebookAccessToken(AccessToken accessToken) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(accessToken.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("facebook login: success", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            if (user != null) {
+                                String userid = user.getUid();
+                                String username = user.getDisplayName();
+                                String email = user.getEmail();
+                                String avatar = String.valueOf(user.getPhotoUrl());
+
+                                mReference = FirebaseDatabase.getInstance().getReference("Users").child(userid);
+                                HashMap<String, String> hashMap = new HashMap<>();
+                                hashMap.put("id", userid);
+                                hashMap.put("username", username);
+                                hashMap.put("avatar", avatar);
+                                hashMap.put("email", email);
+                                hashMap.put("search", username.toLowerCase());
+                                mReference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Intent intent = new Intent(Login.this, HomePage.class);
+                                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                                            startActivity(intent);
+                                            finish();
+                                            progressBar.setVisibility(View.VISIBLE);
+                                        }
+                                    }
+                                });
+                            }
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("facebook login: fails", "signInWithCredential:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+
+
+                });
+    }
+
 
     private void login(String email, String pass) {
 
@@ -113,4 +203,10 @@ public class Login extends AppCompatActivity {
                 });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+
+    }
 }
